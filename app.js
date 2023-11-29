281ab8c9a97a1885d3fcc6ebffd5cd78d8b998bb
Robert Smith <r.reed.smith@gmail.com>
2023-11-28 21:26:16 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import session from 'express-session';
+import morgan from 'morgan';
+import ViteExpress from 'vite-express';
+// import { Movie, User } from './src/model.js';
+
+const app = express();
+const port = '8000';
+ViteExpress.config({ printViteDevServerHost: true });
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
+
+// Custom route middleware function that checks if the user is logged in.
+// function loginRequired(req, res, next) {
+//   if (!req.session.userId) {
+//     res.status(401).json({ error: 'Unauthorized' });
+//   } else {
+//     next();
+//   }
+// }
+
+// app.get('/api/movies/:movieId', async (req, res) => {
+//   const { movieId } = req.params;
+//   const movie = await Movie.findByPk(movieId);
+//   res.json(movie);
+// });
+
+// app.post('/api/auth', async (req, res) => {
+//   const { email, password } = req.body;
+//   const user = await User.findOne({ where: { email: email } });
+
+//   if (user && user.password === password) {
+//     req.session.userId = user.userId;
+//     res.json({ success: true });
+//   } else {
+//     res.json({ success: false });
+//   }
+// });
+
+// Note the `loginRequired` argument passed to the routes below!
+
+ViteExpress.listen(app, port, () => console.log(`Server is listening on http://localhost:${port}`));
